44953f53015dc5d7e4cd610ed5a5d44cab506ce5
Doğukan Caner <76653403+dogukancaner@users.noreply.github.com>
2022-12-12 15:13:40 +0300

    add news api

--- src/pages/News.js
+++ src/pages/News.js
@@ -1,10 +1,25 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const News = () => {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    axios
+      .get(
+        `https://newsapi.org/v2/top-headlines?country=tr&apiKey=89c5ca427aad4671bcefc778642a554c
+    `
+      )
+      .then((res) => {
+        setData(res.data.articles);
+      });
+  });
+
   return (
     <div className=" bg-blue-500 flex flex-col items-center justify-center">
       <div>
-        <h1>News</h1>
+        {data.map((item, index) => {
+          <div></div>;
+        })}
       </div>
       <div>
         <div>Content</div>
