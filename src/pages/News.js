eab11f967caaea29270c0c89dcb7ec6795750cd7
Doğukan Caner <76653403+dogukancaner@users.noreply.github.com>
2022-12-13 17:19:26 +0300

    add news swiper

--- src/pages/News.js
+++ src/pages/News.js
@@ -1,10 +1,13 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
+import { Navigation, Autoplay } from "swiper";
 
 const News = () => {
   const [datas, setDatas] = useState([]);
@@ -20,18 +23,25 @@ const News = () => {
   });
 
   return (
-    <div className=" flex flex-col items-center justify-center">
+    <div>
       <div>
-        {datas.map((data, index) => (
-          <ul key={index}>
-            <li>
-              <img src={data.urlToImage} alt="" />
-            </li>
-          </ul>
-        ))}
-      </div>
-      <div>
-        <div></div>
+        <Swiper
+          spaceBetween={10}
+          centeredSlides={true}
+          autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+          }}
+          navigation={true}
+          modules={[Autoplay, Navigation]}
+          className="text-center"
+        >
+          {datas.map((data, index) => (
+            <SwiperSlide key={index}>
+              <img src={data.urlToImage} width={600} height={600} alt="" />
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </div>
     </div>
   );
