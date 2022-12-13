f2f44daee74645a9a41063fd6c561e642c0adebe
Doğukan Caner <76653403+dogukancaner@users.noreply.github.com>
2022-12-13 22:19:13 +0300

    update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,9 @@
-import React from "react";
 import { toast } from "react-toastify";
+
 import { auth } from "../firebase";
 import { signOut } from "firebase/auth";
 
-const Navbar = ({ users }) => {
+const Navbar = () => {
   const logoutFunc = async () => {
     toast.success("Çıkış Yapılıyor...");
     await signOut(auth);
@@ -11,11 +11,12 @@ const Navbar = ({ users }) => {
       window.location = "/";
     }, 2000);
   };
+
   return (
     <div className="bg-sky-700 h-12 text-white flex  items-center justify-between p-3  ">
       <div className="text-2xl font-bold ">Logo</div>
       <div>
-        <div>Email{users}</div>
+        <div>Email</div>
         <div onClick={logoutFunc} className="cursor-pointer pr-4">
           Logout
         </div>
