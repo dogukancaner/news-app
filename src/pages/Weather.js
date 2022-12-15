ce73a3bce723f10514f11940704e54a155fc4609
Doğukan Caner <76653403+dogukancaner@users.noreply.github.com>
2022-12-15 15:13:19 +0300

    add weather api

--- src/pages/Weather.js
+++ src/pages/Weather.js
@@ -2,15 +2,41 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 
 const Weather = () => {
-  const [weather, setWeather] = useState([]);
+  const apiKey = "9057c725ceb59fb4810e2f46a4d52def";
+  const [weather, setWeather] = useState({});
 
+  const getWeatherDetails = (cityName) => {
+    if (!cityName) return;
+    const apiUrl =
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+      cityName +
+      "&appid=" +
+      apiKey;
+
+    axios
+      .get(apiUrl)
+      .then((res) => {
+        console.log("response", res.data);
+
+        setWeather(res.data);
+      })
+      .catch((err) => {
+        console.log("error", err);
+      });
+  };
+
+  useEffect(() => {
+    getWeatherDetails("İzmir");
+  });
   return (
-    <div className=" bg-red-600 flex flex-col items-center justify-center">
+    <div className=" flex flex-col items-center justify-center">
       <div>
         <h1>Clock</h1>
       </div>
-      <div>
-        <h1>Weather</h1>
+      <div className="border border-solid rounded w-full text-center bg-sky-700 ">
+        <p>{weather?.name}</p>
+        <p>{(weather?.main?.temp - 273.15).toFixed(2)}°C</p>
+        <p>{weather?.weather?.[0]?.main}</p>
       </div>
     </div>
   );
