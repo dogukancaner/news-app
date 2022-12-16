3cc33e932986dbbf930afcad9b0a27fceb1ba742
Doğukan Caner <76653403+dogukancaner@users.noreply.github.com>
2022-12-16 21:35:04 +0300

    add date time

--- src/pages/Weather.js
+++ src/pages/Weather.js
@@ -2,41 +2,34 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 
 const Weather = () => {
-  const apiKey = "9057c725ceb59fb4810e2f46a4d52def";
-  const [weather, setWeather] = useState({});
+  const time = new Date().toLocaleTimeString();
+  const [currentTime, setCurrentTime] = useState(time);
 
-  const getWeatherDetails = (cityName) => {
-    if (!cityName) return;
-    const apiUrl =
-      "https://api.openweathermap.org/data/2.5/weather?q=" +
-      cityName +
-      "&appid=" +
-      apiKey;
+  const date = new Date().toLocaleDateString();
+  const [currentDate, setCurrentDate] = useState(date);
 
-    axios
-      .get(apiUrl)
-      .then((res) => {
-        console.log("response", res.data);
-
-        setWeather(res.data);
-      })
-      .catch((err) => {
-        console.log("error", err);
-      });
+  const updateTime = () => {
+    const newTime = new Date().toLocaleTimeString();
+    setCurrentTime(newTime);
   };
+  setInterval(updateTime, 1000);
 
-  useEffect(() => {
-    getWeatherDetails("İzmir");
-  });
+  const updateDate = () => {
+    const newDate = new Date().toLocaleDateString();
+    setCurrentDate(newDate);
+  };
+  setInterval(updateDate, 1000);
+
   return (
     <div className=" flex flex-col items-center justify-center">
-      <div>
-        <h1>Clock</h1>
+      <div className="border-solid border-2 border-sky-700 p-4 mt-4 mb-4 font-bold text-xl rounded-2xl">
+        <span className="border-r-2 border-sky-700 pr-4 mr-4 ">
+          {currentDate}
+        </span>
+        <span className="">{currentTime}</span>
       </div>
       <div className="border border-solid rounded w-full text-center bg-sky-700 ">
-        <p>{weather?.name}</p>
-        <p>{(weather?.main?.temp - 273.15).toFixed(2)}°C</p>
-        <p>{weather?.weather?.[0]?.main}</p>
+        <h1 className="text-2xl font-bold text-white">Hava Durumu</h1>
       </div>
     </div>
   );
